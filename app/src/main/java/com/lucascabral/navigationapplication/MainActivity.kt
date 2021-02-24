6686e89bd165bb7dbf8d964a89d71536ad056e15
lucascabraldeve <lucas_cabral.deve@outlook.com>
2021-02-24 11:25:59 -0300

    Toolbar configurada.

--- app/src/main/java/com/lucascabral/navigationapplication/MainActivity.kt
+++ app/src/main/java/com/lucascabral/navigationapplication/MainActivity.kt
@@ -2,13 +2,31 @@ package com.lucascabral.navigationapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.lucascabral.navigationapplication.ui.profile.ProfileFragment
 import com.lucascabral.navigationapplication.ui.start.StartFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setSupportActionBar(toolbarMain)
+        navController = findNavController(R.id.navHostFragment)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }

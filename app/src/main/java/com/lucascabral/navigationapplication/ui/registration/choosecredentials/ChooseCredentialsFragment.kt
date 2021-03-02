994057c42c729ecd951237f8eb452acca5933d0a
lucascabraldeve <lucas_cabral.deve@outlook.com>
2021-03-02 16:52:52 -0300

    Fluxo de registro criado com safeargs do navigation component.

---
+++ app/src/main/java/com/lucascabral/navigationapplication/ui/registration/choosecredentials/ChooseCredentialsFragment.kt
@@ -0,0 +1,18 @@
+package com.lucascabral.navigationapplication.ui.registration.choosecredentials
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.lucascabral.navigationapplication.R
+
+class ChooseCredentialsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_choose_credentials, container, false)
+    }
+}
--- app/src/main/java/com/lucascabral/navigationapplication/ui/registration/profile/ProfileDataFragment.kt
+++ app/src/main/java/com/lucascabral/navigationapplication/ui/registration/profile/ProfileDataFragment.kt
@@ -5,14 +5,35 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
 import com.lucascabral.navigationapplication.R
+import com.lucascabral.navigationapplication.ui.registration.RegistrationViewModel
+import kotlinx.android.synthetic.main.fragment_profile_data.*
 
 class ProfileDataFragment : Fragment() {
+
+    private val registrationViewModel: RegistrationViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_profile_data, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        registrationViewModel.registrationStateEvent.observe(viewLifecycleOwner) { registrationState ->
+            when (registrationState) {
+                is RegistrationViewModel.RegistrationState.CollectCredentials -> {
+                    val name = inputProfileDataName.text.toString()
+                    val directions = ProfileDataFragmentDirections
+                        .actionProfileDataFragmentToChooseCredentialsFragment(name)
+                    findNavController().navigate(directions)
+                }
+            }
+        }
     }
 }

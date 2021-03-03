07c48ecff17a0cce1fe56414f35b149c633f1b32
lucascabraldeve <lucas_cabral.deve@outlook.com>
2021-03-03 15:36:50 -0300

    Dados recebidos com safeargs.

--- app/src/main/java/com/lucascabral/navigationapplication/ui/registration/choosecredentials/ChooseCredentialsFragment.kt
+++ app/src/main/java/com/lucascabral/navigationapplication/ui/registration/choosecredentials/ChooseCredentialsFragment.kt
@@ -5,14 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavArgs
+import androidx.navigation.fragment.navArgs
 import com.lucascabral.navigationapplication.R
+import kotlinx.android.synthetic.main.fragment_choose_credentials.*
 
 class ChooseCredentialsFragment : Fragment() {
+
+    private val args: ChooseCredentialsFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_choose_credentials, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        textChooseCredentialsName.text = getString(R.string.choose_credentials_text_name, args.name)
     }
 }

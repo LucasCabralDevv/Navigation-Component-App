9313cc5b995c4bf7c171db8081e3d224b826e851
lucascabraldeve <lucas_cabral.deve@outlook.com>
2021-03-03 14:59:58 -0300

    Validação dos componentes da tela de profile data finalizada.

--- app/src/main/java/com/lucascabral/navigationapplication/ui/registration/profile/ProfileDataFragment.kt
+++ app/src/main/java/com/lucascabral/navigationapplication/ui/registration/profile/ProfileDataFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.textfield.TextInputLayout
 import com.lucascabral.navigationapplication.R
 import com.lucascabral.navigationapplication.ui.registration.RegistrationViewModel
 import kotlinx.android.synthetic.main.fragment_profile_data.*
@@ -25,6 +26,20 @@ class ProfileDataFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val validationFields = initValidationFields()
+        listenToRegistrationViewModelEvents(validationFields)
+
+        buttonProfileDataNext.setOnClickListener {
+            collectProfileData()
+        }
+    }
+
+    private fun initValidationFields() = mapOf(
+        RegistrationViewModel.INPUT_NAME.first to inputLayoutProfileDataName,
+        RegistrationViewModel.INPUT_BIO.first to inputLayoutProfileDataBio
+    )
+
+    private fun listenToRegistrationViewModelEvents(validationFields: Map<String, TextInputLayout>) {
         registrationViewModel.registrationStateEvent.observe(viewLifecycleOwner) { registrationState ->
             when (registrationState) {
                 is RegistrationViewModel.RegistrationState.CollectCredentials -> {
@@ -33,7 +48,18 @@ class ProfileDataFragment : Fragment() {
                         .actionProfileDataFragmentToChooseCredentialsFragment(name)
                     findNavController().navigate(directions)
                 }
+                is RegistrationViewModel.RegistrationState.InvalidProfileData -> {
+                    registrationState.fields.forEach { fieldError ->
+                        validationFields[fieldError.first]?.error = getString(fieldError.second)
+                    }
+                }
             }
         }
+    }
+
+    private fun collectProfileData() {
+        val name = inputProfileDataName.text.toString()
+        val bio = inputProfileDataBio.text.toString()
+        registrationViewModel.collectProfileData(name, bio)
     }
 }
